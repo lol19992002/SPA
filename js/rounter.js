99906433128c70b43de6b36f6e7d4c5ee11a28cf
Mateusz-Melson <matmels@wp.pl>
2025-01-28 17:36:11 +0100

    testy
    .

---
+++ js/rounter.js
@@ -0,0 +1,41 @@
+let pageUrls = {
+    about: '/index.html?about',
+    contact: '/index.html?contact'
+  };
+  
+  function OnStartUp() {
+    popStateHandler();
+  }
+  OnStartUp();
+  
+  document.querySelector('#about-link').addEventListener('click', () => {
+    history.pushState({ page: 'about' }, 'About', '?about');
+    RenderAboutPage();
+  });
+  document.querySelector('#contact-link').addEventListener('click', () => {
+    history.pushState({ page: 'contact' }, 'Contact', '?contact');
+    RenderContactPage();
+  });
+  
+  function RenderAboutPage() {
+    document.querySelector('main').innerHTML = `
+      <h1 class="title">About Me</h1>
+      <p>Here you can learn more about me...</p>
+    `;
+  }
+  
+  function RenderContactPage() {
+    document.querySelector('main').innerHTML = `
+      <h1 class="title">Contact</h1>
+      <p>Feel free to reach out via email or phone...</p>
+    `;
+  }
+  
+  function popStateHandler() {
+    let loc = window.location.href.split(window.location.host)[1];
+    if (loc === pageUrls.about) RenderAboutPage();
+    else if (loc === pageUrls.contact) RenderContactPage();
+  }
+  
+  window.onpopstate = popStateHandler;
+  
